fdc43b2edfea12886b16d9a76c3086b20ebc3791
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-16 14:40:17 -0400

    adding automation

--- bin/automation/driver.py
+++ bin/automation/driver.py
@@ -4,9 +4,11 @@ import subprocess as sp
 import sys
 def main(*args):
     print(args[0])
+    # for testing
+
 if __name__ == "__main__":
-    main(sys.argv)
-    print("functional");
+    #main(sys.argv)
+    # print("functional");
     # sp.call(["sh test.sh"])
     os.system("sh command.sh")
     
