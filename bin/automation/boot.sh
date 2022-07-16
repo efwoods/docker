c14ecec7673eb3b4822bf9a12945b07569702751
efwoods <22337004+efwoods@users.noreply.github.com>
2022-07-16 15:04:37 -0400

    update

--- bin/automation/boot.sh
+++ bin/automation/boot.sh
@@ -1,3 +1,3 @@
 ./listen.sh<myPipe &
-docker run --rm -v $(pwd):/usr/src/app evdev3/auto driver.py>myPipe
+docker run --rm -v $(pwd):/usr/src/app evdev3/auto driver.py>myPipe &
 #./listen.sh<myPipe &
